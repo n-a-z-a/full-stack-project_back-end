9f229817ec7ceb86c978c66c33e04ddf28ff0fd8
n-a-z-a <nadesper@gmail.com>
2022-06-01 12:11:24 +0100

    new commit

--- src/main/java/com/example/favouritetracksbackend/Track.java
+++ src/main/java/com/example/favouritetracksbackend/Track.java
@@ -1,7 +1,15 @@
 package com.example.favouritetracksbackend;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Track {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int trackId;
     private String title;
     private String artist;
--- src/main/java/com/example/favouritetracksbackend/TrackRepository.java
+++ src/main/java/com/example/favouritetracksbackend/TrackRepository.java
@@ -1,7 +1,9 @@
 package com.example.favouritetracksbackend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TrackRepository extends JpaRepository <Track, Integer> {
     int deleteTrackByTrackId(int trackId);
 }
